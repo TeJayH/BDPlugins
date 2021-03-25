deaef245c500471feaff86ea616c1538f44377c4
TeJay <github@tejayh.com>
2021-03-25 10:22:51 -0300

    Fixed ra$

--- Plugins/TheClapBestClapPluginClapEver-Fork/TheClapBestClapPluginClapEver-Fork.plugin.js
+++ Plugins/TheClapBestClapPluginClapEver-Fork/TheClapBestClapPluginClapEver-Fork.plugin.js
@@ -17,16 +17,16 @@ module.exports = (() =>
 					name: "TeJay (Base by Metalloriff)",
 				}
 			],
-			version: "2.1.7",
-			description: "Literally useless, toxic cancer. Triggers are [b$ - Replace B's with the B emoji, block$ - Replace letters with block script, brail$ - Replace letters with brail, bubble$ - Replace letters and numbers with with bubble script, clapclap$ - inserts clap emojis between words, fancy$ - Replace letters with handwritten script, goth$ - Replace letters with the font you'd see on a 40yr olds midlife crisis tattoo, morse$ - Replace letters and numbers with morse code, owo$ - owoifys the text and adds a random UwU face, ra$ - Replaces letters and numbers with the emoji version, reverse$ - Reverses the text, $superclapclap - Adds a clap after every letter, woke$ - TaLkS LiKe tHis]",		},
+			version: "2.1.8",
+			description: "Literally useless, toxic cancer. Triggers are [b$ - Replace B's with the B emoji, block$ - Replace letters with block script, brail$ - Replace letters with brail, bubble$ - Replace letters and numbers with with bubble script, clapclap$ - inserts clap emojis between words, fancy$ - Replace letters with handwritten script, goth$ - Replace letters with the font you'd see on a 40yr olds midlife crisis tattoo, morse$ - Replace letters and numbers with morse code, owo$ - owoifys the text and adds a random UwU face, ra$ - Replaces letters and numbers with the emoji version, reverse$ - Reverses the text, superclapclap$ - Adds a clap after every letter, woke$ - TaLkS LiKe tHis]",		},
 		changelog:
 		[
 			{
-				title: "2.1.7",
+				title: "2.1.8",
 				type: "Changed",
 				items:
 				[
-					"Swapped morse symbols to - and . so online translators can parse it properly."
+					"Fixed ra$ so capitals don't break it."
 				]
 			}
 		]
@@ -72,7 +72,7 @@ module.exports = (() =>
 							case "ra":
 								const ra = (/^ra\$/g).exec(content);
 
-								message.content = message.content.substr(ra[0].length, message.content.length)
+								message.content = message.content.toLowerCase().substr(ra[0].length, message.content.length)
 									.split(" ")
 									.join("\t")
 									.replace(/[A-Za-z]/g, x => ` :regional_indicator_${x}: `);
